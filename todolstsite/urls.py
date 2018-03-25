1773c95ebeda74f64cdc4036b3ecabd9a0e75080
Danny <dan.ramirezrubilar@gmail.com>
2018-03-25 18:57:22 -0300

    Agregar Tareas

--- todolstsite/urls.py
+++ todolstsite/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
 	url(r'^$',views.lista_tareas),
+	url(r'^agregar_tarea$',views.agregar_tarea,name="agregar_tarea"),
 ]
--- todolstsite/views.py
+++ todolstsite/views.py
@@ -1,6 +1,22 @@
 from django.shortcuts import render
 from .models import Tarea
+from .models import Usuario
+from .forms import TareaForm
+from django.shortcuts import redirect
 
 def lista_tareas(request):
 	tareas = Tarea.objects.all();
 	return render(request,'todolstsite/lista_tareas.html',{'tareas':tareas})
+
+def agregar_tarea(request):
+	if request.method=="POST":
+		form = TareaForm(request.POST)
+		if form.is_valid():
+			tarea = form.save(commit=False)
+			tarea.usuario_id = 1
+			tarea.save()
+			return redirect('agregar_tarea')
+	else:
+		form = TareaForm()
+
+	return render(request,'todolstsite/agregar_tarea.html',{'form':form})
